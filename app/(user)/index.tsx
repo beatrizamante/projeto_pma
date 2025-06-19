cd9083352dc4f50bd42b13d7edcb4a5b31a3f4c8
beatrizamante <beatrizamante@hotmail.com>
2025-06-19 17:02:46 -0300

    Fixing cards

--- app/(user)/index.tsx
+++ app/(user)/index.tsx
@@ -2,6 +2,7 @@ import { View, Text, TouchableOpacity, ScrollView } from "react-native";
 import React from "react";
 import { useRouter } from "expo-router";
 import Footer from "../../components/Footer";
+import Card from "../../components/Card";
 
 export default function UserHome() {
   const router = useRouter();
@@ -17,6 +18,16 @@ export default function UserHome() {
         }}
       >
         <View className="flex flex-col justify-center items-center gap-4">
+          <Card
+            title="Manage Videos"
+            content="Look for the person you need to find"
+            uri={require("../../assets/manage_videos.png")}
+          ></Card>
+          <Card
+            title="Finding People"
+            content="Create or manage existing videos"
+            uri={require("../../assets/finding_people.png")}
+          ></Card>
           <View className="flex w-max-[269px] w-full">
             <Text className="pt-6 text-darker font-semibold text-lg">
               If you can’t find a specific person’s hash, please, contact your
--- components/Card.tsx
+++ components/Card.tsx
@@ -10,22 +10,31 @@ import React from "react";
 
 type ImageCardProps = {
   onPress?: () => void;
+  title: string;
   content: string;
   uri: ImageSourcePropType;
 };
 
-export default function Card({
-  onPress,
-  content,
-  uri,
-}: ImageCardProps) {
+export default function Card({ onPress, title, content, uri }: ImageCardProps) {
   return (
     <View className="w-[165px] h-[228px] flex flex-col justify-center items-center border border-darker rounded-[25px] shadow-black shadow-lg bg-semilight">
-      <TouchableOpacity onPress={onPress} className="flex justify-center items-center gap-4">
-        <Image source={uri} className="w-max-[165px] h-max-[120px] px-4 pt-6"></Image>
-        <Text className="text-center text-darker font-semibold p-4">{content}</Text>
+      <TouchableOpacity
+        onPress={onPress}
+        className="flex justify-center items-center gap-4"
+      >
+        <Image
+          source={uri}
+          className="w-max-[165px] h-max-[120px] px-4"
+        ></Image>
+        <View className="gap-2 px-2">
+          <Text className="text-center text-lg text-darker font-semibold">
+            {title}
+          </Text>
+          <Text className="text-center text-darker font-semibold">
+            {content}
+          </Text>
+        </View>
       </TouchableOpacity>
     </View>
   );
 }
-
