171aaa7cccc093b59935e654eef9a36c725c176c
beatrizamante <beatrizamante@hotmail.com>
2025-06-20 15:50:15 -0300

    Bulding user management

--- app/(admin)/userManagement.tsx
+++ app/(admin)/userManagement.tsx
@@ -1,8 +1,9 @@
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, ScrollView, TouchableOpacity } from "react-native";
 import React from "react";
 import { useRouter } from "expo-router";
 import Button from "../../components/Button";
 import Footer from "../../components/Footer";
+import Input from "../../components/form/Input";
 
 export default function userManagement() {
   const router = useRouter();
@@ -20,11 +21,23 @@ export default function userManagement() {
       >
         <View className="flex-1 justify-between items-center px-6">
           <View className="flex flex-col justify-center items-center gap-4 mb-2">
-            <Text className="text-darker text-center font-semibold">
-              Editing {"loged_user"}
-            </Text>
-
-            <Button content="Sign Up!" onPress={() => {}} />
+            <View className="flex flex-row justify-between items-center w-full px-7">
+              <TouchableOpacity className="flex w-[140px]">
+                <Text className="text-lg text-darker font-semibold text-center">
+                  Back
+                </Text>
+              </TouchableOpacity>
+              <Text className="text-darker text-center font-semibold">
+                Editing {"loged_user"}
+              </Text>
+            </View>
+            <Input label="username" value={""} handler={(text: string) => {}} />
+            <Input label="email" value={""} handler={(text: string) => {}} />
+            <Input label="password" value={""} handler={(text: string) => {}} />
+            <Input label="role" value={""} handler={(text: string) => {}} />
+            <View>
+              <Button content="Update user" onPress={() => {}} />
+            </View>
           </View>
         </View>
       </ScrollView>
